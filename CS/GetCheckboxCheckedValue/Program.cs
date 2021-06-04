6f17585cedd3e8244d3251b12ae74639e771bfc8
geumjaLee <polina.fedorova@devexpress.com>
2021-06-04 11:49:23 +0300

    update code snippet with facades

--- CS/GetCheckboxCheckedValue/Program.cs
+++ CS/GetCheckboxCheckedValue/Program.cs
@@ -1,61 +1,28 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using DevExpress.Pdf;
+﻿using DevExpress.Pdf;
 
-namespace GetCheckboxCheckedValue {
-    class Program {
+namespace GetCheckboxCheckedValue
+{
+    class Program
+    {
 
-        static void Main(string[] args) {
-
+        static void Main(string[] args)
+        {
             // Load a document with an interactive form.
             PdfDocumentProcessor processor = new PdfDocumentProcessor();
             processor.LoadDocument("..\\..\\InteractiveForm.pdf");
 
-            // Obtain interactive form data from a document.
-            PdfFormData formData = processor.GetFormData();
+            PdfDocumentFacade documentFacade = processor.DocumentFacade;
+            PdfAcroFormFacade acroForm = documentFacade.AcroForm;
 
-            // Specify a cheched value for the Female form field obtained from the GetCheckboxCheckedValue method.
-            formData["Female"].Value = GetCheckboxCheckedValue(processor, "Female");
+            // Obtain the check box form field:
+            PdfCheckBoxFormFieldFacade genderField = acroForm.GetCheckBoxFormField("Female");
 
-            // Apply data to the interactive form. 
-            processor.ApplyFormData(formData);
+            // Specify a checked value for the Female form field:
+            genderField.IsChecked = true;
 
             // Save the modified document.
             processor.SaveDocument("..\\..\\Result.pdf");
         }
 
-        static string GetCheckboxCheckedValue(PdfDocumentProcessor processor, string fieldName) {
-            if (processor.Document == null || processor.Document.AcroForm == null)
-                return null;
-            PdfInteractiveFormField button = FindCheckBox(processor.Document.AcroForm.Fields, "", fieldName);
-            if (button != null) {
-                PdfAnnotationAppearances appearance = button.Widget.Appearance;
-                if (appearance != null) {
-                    IList<string> names = appearance.Normal.Forms.Keys.ToList();
-                    if (names.Count == 1)
-                        return names.First();
-                    if (names.Count > 1)
-                        return names[0] == "Off" ? names[1] : names[0];
-                }
-                return null;
-            }
-            return null;
-        }
-
-        static PdfInteractiveFormField FindCheckBox(IEnumerable<PdfInteractiveFormField> fields, string partialName, string fieldName) {
-            foreach (PdfInteractiveFormField field in fields) {
-                if (partialName + field.Name == fieldName) {
-                    if (field.Flags.HasFlag(PdfInteractiveFormFieldFlags.Radio) || field.Flags.HasFlag(PdfInteractiveFormFieldFlags.PushButton) || field.Widget == null)
-                        return null;
-                    return field as PdfButtonFormField;
-                }
-                if (field.Kids != null) {
-                    PdfInteractiveFormField intermediateResult = FindCheckBox(field.Kids, partialName + "." + field.Name, fieldName);
-                    if (intermediateResult != null)
-                        return intermediateResult;
-                }
-            }
-            return null;
-        }
     }
 }
--- VB/GetCheckboxCheckedValue/Program.vb
+++ VB/GetCheckboxCheckedValue/Program.vb
@@ -1,66 +1,25 @@
-﻿Imports System.Collections.Generic
-Imports System.Linq
-Imports DevExpress.Pdf
+﻿Imports DevExpress.Pdf
 
 Namespace GetCheckboxCheckedValue
-    Friend Class Program
+	Friend Class Program
 
-        Shared Sub Main(ByVal args() As String)
+		Shared Sub Main(ByVal args() As String)
+			' Load a document with an interactive form.
+			Dim processor As New PdfDocumentProcessor()
+			processor.LoadDocument("..\..\InteractiveForm.pdf")
 
-            ' Load a document with an interactive form.
-            Dim processor As New PdfDocumentProcessor()
-            processor.LoadDocument("..\..\InteractiveForm.pdf")
+			Dim documentFacade As PdfDocumentFacade = processor.DocumentFacade
+			Dim acroForm As PdfAcroFormFacade = documentFacade.AcroForm
 
-            ' Obtain interactive form data from a document.
-            Dim formData As PdfFormData = processor.GetFormData()
+			' Obtain the check box form field:
+			Dim genderField As PdfCheckBoxFormFieldFacade = acroForm.GetCheckBoxFormField("Female")
 
-            ' Specify a cheched value for the Female form field obtained from the GetCheckboxCheckedValue method.
-            formData("Female").Value = GetCheckboxCheckedValue(processor, "Female")
+			' Specify a checked value for the Female form field:
+			genderField.IsChecked = True
 
-            ' Apply data to the interactive form. 
-            processor.ApplyFormData(formData)
+			' Save the modified document.
+			processor.SaveDocument("..\..\Result.pdf")
+		End Sub
 
-            ' Save the modified document.
-            processor.SaveDocument("..\..\Result.pdf")
-        End Sub
-
-        Private Shared Function GetCheckboxCheckedValue(ByVal processor As PdfDocumentProcessor, ByVal fieldName As String) As String
-            If processor.Document Is Nothing OrElse processor.Document.AcroForm Is Nothing Then
-                Return Nothing
-            End If
-            Dim button As PdfInteractiveFormField = FindCheckBox(processor.Document.AcroForm.Fields, "", fieldName)
-            If button IsNot Nothing Then
-                Dim appearance As PdfAnnotationAppearances = button.Widget.Appearance
-                If appearance IsNot Nothing Then
-                    Dim names As IList(Of String) = appearance.Normal.Forms.Keys.ToList()
-                    If names.Count = 1 Then
-                        Return names.First()
-                    End If
-                    If names.Count > 1 Then
-                        Return If(names(0) = "Off", names(1), names(0))
-                    End If
-                End If
-                Return Nothing
-            End If
-            Return Nothing
-        End Function
-
-        Private Shared Function FindCheckBox(ByVal fields As IEnumerable(Of PdfInteractiveFormField), ByVal partialName As String, ByVal fieldName As String) As PdfInteractiveFormField
-            For Each field As PdfInteractiveFormField In fields
-                If partialName & field.Name = fieldName Then
-                    If field.Flags.HasFlag(PdfInteractiveFormFieldFlags.Radio) OrElse field.Flags.HasFlag(PdfInteractiveFormFieldFlags.PushButton) OrElse field.Widget Is Nothing Then
-                        Return Nothing
-                    End If
-                    Return TryCast(field, PdfButtonFormField)
-                End If
-                If field.Kids IsNot Nothing Then
-                    Dim intermediateResult As PdfInteractiveFormField = FindCheckBox(field.Kids, partialName & "." & field.Name, fieldName)
-                    If intermediateResult IsNot Nothing Then
-                        Return intermediateResult
-                    End If
-                End If
-            Next field
-            Return Nothing
-        End Function
-    End Class
+	End Class
 End Namespace
