76dae02369f930ccb9c847abbf3db4412c5fb367
DevExpressExampleBot <fakeEmail>
2022-02-02 11:12:48 +0300

    Source auto update [skip ci]

--- VB/GetCheckboxCheckedValue/Program.vb
+++ VB/GetCheckboxCheckedValue/Program.vb
@@ -1,25 +1,21 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace GetCheckboxCheckedValue
-	Friend Class Program
 
-		Shared Sub Main(ByVal args() As String)
-			' Load a document with an interactive form.
-			Dim processor As New PdfDocumentProcessor()
-			processor.LoadDocument("..\..\InteractiveForm.pdf")
+    Friend Class Program
 
-			Dim documentFacade As PdfDocumentFacade = processor.DocumentFacade
-			Dim acroForm As PdfAcroFormFacade = documentFacade.AcroForm
-
-			' Obtain the check box form field:
-			Dim genderField As PdfCheckBoxFormFieldFacade = acroForm.GetCheckBoxFormField("Female")
-
-			' Specify a checked value for the Female form field:
-			genderField.IsChecked = True
-
-			' Save the modified document.
-			processor.SaveDocument("..\..\Result.pdf")
-		End Sub
-
-	End Class
+        Shared Sub Main(ByVal args As String())
+            ' Load a document with an interactive form.
+            Dim processor As PdfDocumentProcessor = New PdfDocumentProcessor()
+            processor.LoadDocument("..\..\InteractiveForm.pdf")
+            Dim documentFacade As PdfDocumentFacade = processor.DocumentFacade
+            Dim acroForm As PdfAcroFormFacade = documentFacade.AcroForm
+            ' Obtain the check box form field:
+            Dim genderField As PdfCheckBoxFormFieldFacade = acroForm.GetCheckBoxFormField("Female")
+            ' Specify a checked value for the Female form field:
+            genderField.IsChecked = True
+            ' Save the modified document.
+            processor.SaveDocument("..\..\Result.pdf")
+        End Sub
+    End Class
 End Namespace
